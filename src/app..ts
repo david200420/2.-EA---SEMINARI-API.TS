66c2e483273222246c163bdebd02c28df113b202
Bryan Garcia Martin <bryan.garcia@estudiantat.upc.edu>
2025-09-21 16:57:53 +0200

    app.ts creado para conexion a mongodb

---
+++ src/app..ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import mongoose from "mongoose";
+import cors from 'cors'; 
+
+
+const app = express();
+const PORT = 3000;
+
+//////////////////////AQUI APLICAMOS LAS VARIABLES PARA EL MIDDLE WARE CORS//////////////////////
+app.use(cors());
+app.use(express.json());
+
+//////////////////////AQUI CONECTAMOS A LA BASE DE DATOS//////////////////////
+mongoose.connect('mongodb://localhost:27017/BBDD')
+    .then(() => {
+        console.log('CONEXION EXITOSA A LA BASE DE DATOS DE MONGODB'); 
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    })
+    .catch(err => {
+        console.error('HAY ALGUN ERROR CON LA CONEXION', err);
+    });
+
+//////////FALTAN PONER LAS RUTAS: ALGO TIPO app.routes()//////////////////////
+
